fcce97510d3dda56845ec2ffeb9e13c334b9dbe8
sojida <sojidaniels@gmail.com>
2019-02-04 02:56:20 +0100

    feat(fetch)Admin can create party - admin can create party - admin can get all parties [delivers #163685350]

--- scripts/deleteModal.js
+++ scripts/deleteModal.js
@@ -1,25 +1,28 @@
-const delModal = document.getElementById("delete-modal");
-const delPartyBtn = document.querySelectorAll(".delete-party");
-const delClose = document.getElementsByClassName("del-close")[0];
-const no = document.querySelector('.no')
-
-
-delPartyBtn.forEach((btn) => {
-    btn.addEventListener('click' , () => {
-        delModal.style.display = "block";
-    })
-})
- 
-delClose.onclick = function() {
-  delModal.style.display = "none";
-}
-
-no.onclick = function() {
+setTimeout(()=>{
+  console.log(fetchTime)
+  const delModal = document.getElementById("delete-modal");
+  const delPartyBtn = document.querySelectorAll(".delete-party");
+  const delClose = document.getElementsByClassName("del-close")[0];
+  const no = document.querySelector('.no')
+  
+  
+  delPartyBtn.forEach((btn) => {
+      btn.addEventListener('click' , () => {
+          delModal.style.display = "block";
+      })
+  })
+   
+  delClose.onclick = function() {
     delModal.style.display = "none";
   }
-
-  window.addEventListener('click', (event) => {
-    if (event.target == delModal) {
+  
+  no.onclick = function() {
       delModal.style.display = "none";
     }
-});
+  
+    window.addEventListener('click', (event) => {
+      if (event.target == delModal) {
+        delModal.style.display = "none";
+      }
+  });
+}, 2000)
--- scripts/errorModal.js
+++ scripts/errorModal.js
@@ -1,6 +1,9 @@
 const errModal = document.getElementById('error-modal');
+const successModal = document.getElementById('success-modal')
 const errClose = document.getElementsByClassName("err-close")[0];
+const sccClose = document.getElementsByClassName("scc-close")[0];
 const errmsg = document.getElementById('err-msg')
+const sccmsg = document.getElementById('scc-msg')
 
 errClose.onclick = function() {
     errModal.style.display = "none";
@@ -9,5 +12,15 @@ errClose.onclick = function() {
   window.addEventListener('click', (event) => {
     if (event.target === errModal) {
       errModal.style.display = "none";
+    }
+  }) 
+
+  sccClose.onclick = function() {
+    successModal.style.display = "none";
+  }
+  
+  window.addEventListener('click', (event) => {
+    if (event.target === successModal) {
+      successModal.style.display = "none";
     }
   }) 
